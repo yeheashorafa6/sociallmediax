28ea58d9c5046483bcf240c25f0f7bd68ebfb29b
Yehea Shorafa <yeheashorafa6@gmail.com>
2025-02-12 21:24:25 +0200

    finsh front end side and start build in the back end ❤️😊

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -6,7 +6,7 @@ const Comments = () => {
     <div className=''>
       <form className='flex items-center justify-between gap-4 p-4 '>
         <div className='relative w-10 h-10 rounded-full overflow-hidden'>
-          <Image src="general/avatar.png" alt="Lama Dev" width={100} height={100} tr={true}/>
+          <Image src="general/user-avatar.png" alt="Lama Dev" width={100} height={100} tr={true}/>
         </div>
         <input type="text" className="flex-1 bg-transparent outline-none p-2 text-xl" placeholder="Post your reply"/>
         <button className="py-2 px-4 font-bold bg-white text-black rounded-full">Reply</button>
