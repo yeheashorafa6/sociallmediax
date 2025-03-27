755adc4e7eaded62628de1ac2c03d5db62d13702
Yehea Shorafa <yeheashorafa6@gmail.com>
2025-03-27 02:36:33 +0200

    editing signUp page

---
+++ src/app/sign-up/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "SignUp Page",
+  description: "Generated SignUp Page  by Next.js",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
